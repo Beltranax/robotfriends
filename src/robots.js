17c82cb9f5b375d6b767ae725efbd70839e38d33
Angel Beltran <beltranangel7293@gmail.com>
2022-01-22 15:37:08 -0800

    initial commit

---
+++ src/robots.js
@@ -0,0 +1,73 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Enyel Beltranax',
+        username: 'SamuraiSniper',
+        email: '123@gmail.com'
+
+    },
+    {
+        id: 2,
+        name: 'Nemo Rivers',
+        username: 'Jarr',
+        email: 'jarr@gmail.com'
+
+    },
+    {
+        id: 3,
+        name: 'Lencho Flowers',
+        username: 'PigMX',
+        email: 'pig@gmail.com'
+
+    },
+    {
+        id: 4,
+        name: 'Bruse Shark',
+        username: 'Sharkyboy',
+        email: 'halfhsark@gmail.com'
+
+    },
+    {
+        id: 5,
+        name: 'Billy Ape',
+        username: 'ApeBilly123',
+        email: 'planetape@gmail.com'
+
+    },
+    {
+        id: 6,
+        name: 'Toston Virgin',
+        username: 'idiotte',
+        email: '50@gmail.com'
+
+    },
+    {
+        id: 7,
+        name: 'DryMouth',
+        username: '420',
+        email: '420@gmail.com'
+
+    },
+    {
+        id: 8,
+        name: 'Color index',
+        username: 'Color index',
+        email: 'CI123@gmail.com'
+
+    },
+    {
+        id: 9,
+        name: 'goku',
+        username: 'Kakarot',
+        email: '123@gmail.com'
+
+    },
+    {
+        id: 10,
+        name: 'vegeta',
+        username: 'prince',
+        email: '123@gmail.com'
+
+    }
+
+]
